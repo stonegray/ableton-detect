d5df133001e78b228f729106265a3ff8a794fd12
Stone Gray <stonegray@Stones-MacBook-Pro.local>
2020-11-12 02:31:35 -0500

    Housekeeping: Use eslint

--- src/getLicenceInfo.js
+++ src/getLicenceInfo.js
@@ -14,115 +14,114 @@ import os from 'os';
 
 // Update these are required:
 const map = {
-    distrobution: {
-        0x50: "Standard"
-    },
-    product: {
-        "0000": "Ableton Live Suite",
-        "0004": "Ableton Live Lite"
-    }
-}
+	distrobution: {
+		0x50: 'Standard'
+	},
+	product: {
+		'0000': 'Ableton Live Suite',
+		'0004': 'Ableton Live Lite'
+	}
+};
 
 export default async function getLicencesByVersion(version) {
 
 
-    const licenceFilePath = path.join(
-        os.homedir(),
-        "./Library/Application Support/Ableton/",
-        `Live ${version.major}.${version.minor}.${version.patch}`,
-        "./Unlock/Unlock.cfg"
-    );
+	const licenceFilePath = path.join(
+		os.homedir(),
+		'./Library/Application Support/Ableton/',
+		`Live ${version.major}.${version.minor}.${version.patch}`,
+		'./Unlock/Unlock.cfg'
+	);
 
-    // If we can't open the file, just return an empty array.
-    let fileContents;
-    try {
-        fileContents = await fs.promises.readFile(licenceFilePath);
-    } catch (e) {
-        // Unable to access file, skip for now.
-        return [];
-    }
-
-
-    // Start signature of each licence field:
-
-    let position = 1;
-    let bufs = [];
-
-    while (position > 0) {
-        position = fileContents.indexOf(Buffer.from('0000074C6963656E6365', 'hex'), position + 1);
-
-        // Capture 40+1 bytes from offset position+10, and additional 160 bytes for Response
-        if (position > 1) bufs.push(fileContents.slice(position + 10, position + 51 + 164));
-    }
-
-    const licences = [];
-    for (const [index, buf] of bufs.entries()) {
-
-        let licence = {
-        };
-
-        // Position in file:
-        licence.logicalId = index;
-
-        // read encoded index, uint16 directly after LICENCE:
-        licence.licenceId = buf.readInt16LE(0);
+	// If we can't open the file, just return an empty array.
+	let fileContents;
+	try {
+		fileContents = await fs.promises.readFile(licenceFilePath);
+	} catch (e) {
+		// Unable to access file, skip for now.
+		return [];
+	}
 
 
-        // Read product type:
-        licence.productIdRaw = Buffer.from([buf[29], buf[28]])
-        licence.productId = licence.productIdRaw.toString('hex').toUpperCase();
+	// Start signature of each licence field:
 
-        if (map.product[licence.productId]) {
-            licence.productString = map.product[licence.productId];
-        } else {
-            licence.productString = "Unknown Addon 0x" + licence.productId
-        }
+	let position = 1;
+	let bufs = [];
+
+	while (position > 0) {
+		position = fileContents.indexOf(Buffer.from('0000074C6963656E6365', 'hex'), position + 1);
+
+		// Capture 40+1 bytes from offset position+10, and additional 160 bytes for Response
+		if (position > 1) bufs.push(fileContents.slice(position + 10, position + 51 + 164));
+	}
+
+	const licences = [];
+	for (const [index, buf] of bufs.entries()) {
+
+		let licence = {
+		};
+
+		// Position in file:
+		licence.logicalId = index;
+
+		// read encoded index, uint16 directly after LICENCE:
+		licence.licenceId = buf.readInt16LE(0);
 
 
-        // Read serial number:
+		// Read product type:
+		licence.productIdRaw = Buffer.from([buf[29], buf[28]]);
+		licence.productId = licence.productIdRaw.toString('hex').toUpperCase();
 
-        /* I'm not certain we're reading this correctly because it doesn't
+		if (map.product[licence.productId]) {
+			licence.productString = map.product[licence.productId];
+		} else {
+			licence.productString = 'Unknown Addon 0x' + licence.productId;
+		}
+
+
+		// Read serial number:
+
+		/* I'm not certain we're reading this correctly because it doesn't
         match up with the .auz file we have. */
 
-        /* The .cfg file appears to show SerialNumber as a a 6-entry array,
+		/* The .cfg file appears to show SerialNumber as a a 6-entry array,
         which matches what we expect. */
         
-        const serialBytes = [
-            buf.slice(4, 4+2),
-            buf.slice(8, 8+2),
-            buf.slice(12, 12+2),
-            buf.slice(16, 16+2),
-            buf.slice(20, 20+2),
-            buf.slice(24, 24+2)
-        ];
-        licence.serial = serialBytes
-            .map(b => b.toString('hex'))
-            .join('-')
-            .toUpperCase();
+		const serialBytes = [
+			buf.slice(4, 4+2),
+			buf.slice(8, 8+2),
+			buf.slice(12, 12+2),
+			buf.slice(16, 16+2),
+			buf.slice(20, 20+2),
+			buf.slice(24, 24+2)
+		];
+		licence.serial = serialBytes
+			.map(b => b.toString('hex'))
+			.join('-')
+			.toUpperCase();
 
-        // Read the "DistrobutionType"
-        // We can infer from the header structure 
-        let dm = buf[40]; //buf.slice(40,41);
-        licence.distrobutionType = dm;
-        /*
+		// Read the "DistrobutionType"
+		// We can infer from the header structure 
+		let dm = buf[40]; //buf.slice(40,41);
+		licence.distrobutionType = dm;
+		/*
         if (map.distrobution[dm]) {
             licence.distrobutionTypeString = map.distrobution[dm];
         } else {
             licence.distrobutionTypeString = "Unknown"
         }*/
 
-        let rc = buf.slice(44, 44 + 160);
+		let rc = buf.slice(44, 44 + 160);
 
-        licence.response = rc;
+		licence.response = rc;
 
-        // Remove .raw object as we're parsing everything now 
-        //licence.raw = buf;
+		// Remove .raw object as we're parsing everything now 
+		//licence.raw = buf;
 
-        licences.push(licence);
-    }
+		licences.push(licence);
+	}
 
-    console.log(licences);
-    return licences;
+	return licences;
 }
 
 /*
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import plist from 'plist';
 import semver from 'semver';
 
 import getHeaderBytes from './getHeaderBytes.js';
-import getLicenceInfo from './getLicenceInfo.js';
 import getLicencesByVersion from './getLicenceInfo.js';
 
 // Search given paths and get information about installed Ableton versions.
@@ -20,8 +19,8 @@ export default async function getAbletons(searchDirectories) {
 	if (!searchDirectories) {
 		searchDirectories = [
 			'/Applications',
-			path.join(os.homedir(), "./Applications")
-		]
+			path.join(os.homedir(), './Applications')
+		];
 	}
 
 	// Array of all applications detected in folder
@@ -63,39 +62,39 @@ export default async function getAbletons(searchDirectories) {
 		if (!app.name.match(regex)) continue;
 
 		// Read the plist file:
-		const plistFile = path.join(app.dir, app.name, "./Contents/Info.plist");
+		const plistFile = path.join(app.dir, app.name, './Contents/Info.plist');
 
 		let plistData;
 		try {
 			plistData = plist.parse(await fs.promises.readFile(plistFile, 'utf8'));
 		} catch(e){
-			console.warn("Failed to parse Live instance, failed to read Info.plist");
+			console.warn('Failed to parse Live instance, failed to read Info.plist');
 			plistData = {};
 		}
 
 		// By now we have an Ableton, so let's collect some info about it.
 		const info = {
-			relPath: "",
-			absPath: "",
-			version: "",
-			fullVersion: "",
-			minSystemVersion: "",
-			language: "",
-			variant: "",
-			icon: "",
+			relPath: '',
+			absPath: '',
+			version: '',
+			fullVersion: '',
+			minSystemVersion: '',
+			language: '',
+			variant: '',
+			icon: '',
 			licences: null,
 			ok: false,
 			errors: []
-		}
+		};
 
 		// Check if this is actually Live; not another application renamed. If there's a matching
 		// CFBundleIdentifier we can probably skip sanity checking the rest. 
 		if (typeof plistData.CFBundleIdentifier == 'undefined') {
-			console.warn("Failed to parse Live instance, malformed Info.plist");
+			console.warn('Failed to parse Live instance, malformed Info.plist');
 			continue;
 		}
-		if (plistData.CFBundleIdentifier !== "com.ableton.live") {
-			console.warn("Failed to parse Live instance, incorrect CFBundleIdentifier");
+		if (plistData.CFBundleIdentifier !== 'com.ableton.live') {
+			console.warn('Failed to parse Live instance, incorrect CFBundleIdentifier');
 			continue;
 		}
 
@@ -104,7 +103,7 @@ export default async function getAbletons(searchDirectories) {
 		// dumb and didn't bother checking an obvious .cfg file
 		const installInfo = JSON.parse(
 			await fs.promises.readFile(
-				path.join(app.dir, app.name, "./Contents/Resources/Installation.cfg")
+				path.join(app.dir, app.name, './Contents/Resources/Installation.cfg')
 			)
 		);
 
@@ -123,44 +122,44 @@ export default async function getAbletons(searchDirectories) {
 
 		// Get the icon path so we can show the correct icons for different versions/varients
 		// like the green icon for Intro.
-		info.icon = path.join(app.dir, app.name, "./Contents/Resources", plistData.CFBundleIconFile);
+		info.icon = path.join(app.dir, app.name, './Contents/Resources', plistData.CFBundleIconFile);
 
 		// Check if binary is 32-bit or 64-bit:
-		const executablePath = path.join(app.dir, app.name, "./Contents/MacOS/Live");
+		const executablePath = path.join(app.dir, app.name, './Contents/MacOS/Live');
 		const header = await getHeaderBytes(executablePath, 8);
 
 		const headerBytesHex = header.toString('hex');
 
-        // Architecture values defined here:
+		// Architecture values defined here:
 		// https://opensource.apple.com/source/cctools/cctools-836/include/mach/machine.h
 
 		// First int (magic) is defined here:
-		https://opensource.apple.com/source/xnu/xnu-1456.1.26/EXTERNAL_HEADERS/mach-o/loader.h
+		// https://opensource.apple.com/source/xnu/xnu-1456.1.26/EXTERNAL_HEADERS/mach-o/loader.h
 
 		switch (headerBytesHex) {
 
-			// MH_CIGAM_64, CPU_TYPE_I386 | CPU_ARCH_ABI64
-			case 'cffaedfe07000001':
+		// MH_CIGAM_64, CPU_TYPE_I386 | CPU_ARCH_ABI64
+		case 'cffaedfe07000001':
 			// MH_MAGIC_64, CPU_TYPE_I386 | CPU_ARCH_ABI64
-			case 'cafebabe07000001':
-				info.arch = ["x64"];	
-				break;
+		case 'cafebabe07000001':
+			info.arch = ['x64'];	
+			break;
 
 			// MH_CIGAM, CPU_TYPE_I386
-			case 'cefaedfe07000000':
+		case 'cefaedfe07000000':
 			// MH_MAGIC, CPU_TYPE_I386
-			case 'cafebabe07000000':
-				info.arch = ["x32"];	
-				break;
+		case 'cafebabe07000000':
+			info.arch = ['x32'];	
+			break;
 
 			// MH_CIGAM_64, CPU_TYPE_ARM | CPU_ARCH_ABI64
-			case 'cffaedfe12000001':
-				info.arch = ["arm64"];	
-				break;
+		case 'cffaedfe12000001':
+			info.arch = ['arm64'];	
+			break;
 		
-			default:
-				info.errors.push('Unknown architecture: '+headerBytesHex.toUpperCase);
-				break;
+		default:
+			info.errors.push('Unknown architecture: '+headerBytesHex.toUpperCase);
+			break;
 		}
 
 
@@ -169,24 +168,24 @@ export default async function getAbletons(searchDirectories) {
 			semver.coerce(macosRelease().version),
 			semver.coerce('10.15.0'),
 		)){
-			if (info.arch.includes("x32")){
-				info.errors.push('Current platform does not support 32-bit Ableton')
+			if (info.arch.includes('x32')){
+				info.errors.push('Current platform does not support 32-bit Ableton');
 			}
-		};
+		}
 
 		// Detect unusable instance, arm64 when =< Big Sur
 		if (semver.lt(
 			semver.coerce(macosRelease().version),
 			semver.coerce('11.0.0'),
 		)){
-			if (info.arch.includes("arm64")){
-				info.errors.push('Current OS does not support arm64 binaries')
+			if (info.arch.includes('arm64')){
+				info.errors.push('Current OS does not support arm64 binaries');
 			}
-		};
+		}
 
 		// Detect x64 on x32, which is always unsupported
-		if (os.arch() == "x32" && info.arch.includes("x64")){
-				info.errors.push('64-bit binaries are not supported on 32-bit OSs')
+		if (os.arch() == 'x32' && info.arch.includes('x64')){
+			info.errors.push('64-bit binaries are not supported on 32-bit OSs');
 		}
 
 
@@ -197,7 +196,7 @@ export default async function getAbletons(searchDirectories) {
 			semver.coerce(info.minSystemVersion)
 		)){
 			info.errors.push(`Unsupported OS, needs ${info.minSystemVersion} or newer`);
-		};
+		}
 
 
 		// Get licence information (Experimental)
